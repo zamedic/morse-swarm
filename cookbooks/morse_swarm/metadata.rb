d3c8cb9952269c585a4e22a2b46bf57be7a5157e
Marc Arndt <marc@marcarndt.com>
2017-05-30 16:21:26 +0200

    fixing delivery issues

--- cookbooks/morse_swarm/metadata.rb
+++ cookbooks/morse_swarm/metadata.rb
@@ -6,8 +6,15 @@ description      'Installs/Configures Morse Swarm'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-issues_url ''
-source_url ''
+chef_version '>= 12.5' if respond_to?(:chef_version)
+
+
+supports 'centos'
+supports 'redhat'
+supports 'ubuntu'
+
+issues_url 'https://github.com/zamedic/morse-swarm/issues'
+source_url 'https://github.com/zamedic/morse-swarm'
 
 depends 'chef-vault'
 depends 'java'
