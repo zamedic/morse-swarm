3059debabafc9b03d781578ec0e4426f7be826d5
Marc Arndt <marc@marcarndt.com>
2017-06-27 08:20:20 +0200

    fixing recipe

--- cookbooks/morse_swarm/recipes/aws.rb
+++ cookbooks/morse_swarm/recipes/aws.rb
@@ -2,7 +2,9 @@ remote_file "#{Chef::Config[:file_cache_path]}/xray.rpm" do
   source 'https://s3.dualstack.us-east-1.amazonaws.com/aws-xray-assets.us-east-1/xray-daemon/aws-xray-daemon-2.x.rpm'
 end
 
-execute "yum install -y #{Chef::Config[:file_cache_path]}/xray.rpm"
+execute "yum install -y #{Chef::Config[:file_cache_path]}/xray.rpm" do
+  expect [0,1]
+end
 
 package 'awslogs'
 
