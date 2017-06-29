c79a77787ad96a479123d4a25da2945305e039a7
Marc Arndt <marc@marcarndt.com>
2017-06-29 16:42:42 +0200

    bump

--- .delivery/build_cookbook/attributes/default.rb
+++ .delivery/build_cookbook/attributes/default.rb
@@ -2,3 +2,5 @@ default['coffee-truck']['release']['email'] = 'marc@marcarndt.com'
 default['coffee-truck']['release']['user'] = 'Marc Arndt'
 default['delivery']['config']['truck']['update_dependencies']['include'] = 'com.marcarndt.morse:*'
 default['delivery']['config']['truck']['update_dependencies']['active'] = true
+default['delivery']['config']['truck']['codacy']['upload'] = true
+default['delivery']['config']['truck']['codacy']['token'] = '32f2df556aee4a2d94fcc12f806a5a08'
