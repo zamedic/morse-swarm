2fd41540d9baf7c9e6d1e1f692c96bdb22bc6d69
Marc Arndt <marc@marcarndt.com>
2017-07-02 19:56:30 +0200

    switching docker to default to it runs as root

--- .delivery/build_cookbook/recipes/default.rb
+++ .delivery/build_cookbook/recipes/default.rb
@@ -5,13 +5,6 @@
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 include_recipe 'maven_sonatype_truck::default'
 
-docker_installation 'default' do
-  action :create
-end
-secrets = get_project_secrets
-
-docker_registry 'https://index.docker.io/v1/' do
-  username 'zamedic'
-  password secrets['docker-password']
-  email 'marc@marcarndt.com'
+if(node['delivery']['change']['stage'] == 'acceptance' && node['delivery']['change']['phase'] == 'provision')
+  include_recipe 'docker'
 end
--- .delivery/build_cookbook/recipes/publish.rb
+++ .delivery/build_cookbook/recipes/publish.rb
@@ -4,15 +4,7 @@
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 
-docker_image 'morse_swarm' do
-  tag 'morse_swarm'
-  source "#{node['delivery']['workspace']['repo']}/Dockerfile"
-  action :build
-end
 
-docker_image 'morse_swarm' do
-  action :push
-end
 
 
 
